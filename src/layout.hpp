f75b533f7d8c62f9afe2357b92b01be098776eba
tigmit <tim_z@t-online.de>
2024-06-16 13:49:21 +0200

    working state with new wallpaper

--- src/layout.hpp
+++ src/layout.hpp
@@ -19,10 +19,10 @@ int layout1[numLayers][numRows][numCols]
 {
     {
         {KEY_ESC,             NC,       KEY_F1,  KEY_F2,  KEY_F3,  KEY_F4,  KEY_F5,  KEY_F6,  KEY_F7,        KEY_F8,   KEY_F9,  KEY_F10,         KEY_F11,       KEY_F12,      KEY_PRTSC,             NC},
-        {'~',                '1',          '2',     '3',     '4',     '5',     '6',     '7',     '8',           '9',      '0',      '-',             '+', KEY_BACKSPACE,            ' ',             NC},
-        {KEY_TAB,            'q',          'w',     'e',     'r',     't',     'z',     'u',     'i',           'o',      'p',      '{',             '}',          '\\',     KEY_DELETE,             NC},
-        {KEY_CAPS_LOCK,      'a',          's',     'd',     'f',     'g',     'h',     'j',     'k',           'l',      'o',      'a',              NC,    KEY_RETURN,             NC,KEY_RIGHT_ARROW},
-        {KEY_LEFT_SHIFT,     'y',          'x',     'c',     'v',     'b',     'n',     'm',     ',',           '.',      '-',       NC, KEY_RIGHT_SHIFT,            NC,             NC,   KEY_UP_ARROW},
+        {'`',                '1',          '2',     '3',     '4',     '5',     '6',     '7',     '8',           '9',      '0',      '-',             '=', KEY_BACKSPACE,            ' ',             NC},
+        {KEY_TAB,            'q',          'w',     'e',     'r',     't',     'y',     'u',     'i',           'o',      'p',      '[',             ']',          '\\',     KEY_DELETE,             NC},
+        {KEY_CAPS_LOCK,      'a',          's',     'd',     'f',     'g',     'h',     'j',     'k',           'l',      ';',     '\'',              NC,    KEY_RETURN,             NC,KEY_RIGHT_ARROW},
+        {KEY_LEFT_SHIFT,     'z',          'x',     'c',     'v',     'b',     'n',     'm',     ',',           '.',      '/',       NC, KEY_RIGHT_SHIFT,            NC,             NC,   KEY_UP_ARROW},
         {KEY_LEFT_CTRL, KEY_HOME, KEY_LEFT_ALT,     NC ,      NC,     ' ',      NC,      NC,      NC, KEY_RIGHT_ALT, KEY_FN_1, KEY_FN_2,  KEY_RIGHT_CTRL,            NC, KEY_LEFT_ARROW, KEY_DOWN_ARROW}
     } 
 };
