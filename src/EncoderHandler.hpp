6844a840107581a0bcf167a30a4c752724bd86ff
tigmit <tim_z@t-online.de>
2024-07-28 20:40:43 +0200

    introducing FSM statemachine

--- src/EncoderHandler.hpp
+++ src/EncoderHandler.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "Defines/hardwareDef.hpp"
+#include "FsmStates.hpp"
 #include "KeyboardHandler.hpp"
 #include "debugSettings.hpp"
 #include <Arduino.h>
@@ -22,7 +23,7 @@ public:
     pinMode(encBTN, INPUT);
   }
 
-  void updateVolume() {
+  void updateVolume() { // perhaps legacy code
     VolPosition = encoder.getCount() / 2;
     if (VolPosition > 0) {
       kbd.write(KEY_MEDIA_VOLUME_UP);
@@ -44,23 +45,7 @@ public:
     }
   }
 
-  void printPosition() { // legacy code
-    VolPosition = encoder.getCount() / 2;
-    Serial.println(VolPosition);
-    if (digitalRead(encBTN) == LOW) {
-      Serial.println("Button pressed");
-    }
-  }
-
-  int UpdateModeSelect() {
-
-    // check if the button was pressed
-    if (digitalRead(encBTN) == LOW && !ecnBtnPressed) {
-      ecnBtnPressed = true;
-      modeSelected = modeSet;
-    } else if (digitalRead(encBTN) == HIGH && ecnBtnPressed) {
-      ecnBtnPressed = false;
-    }
+  void UpdateMainStateSelect(int &modeSet) {
 
     // checking encoder val state
     modeEncPosition = encoder.getCount() / 2;
@@ -68,33 +53,73 @@ public:
       // do nothing
     } else if (modeEncPosition > 0) {
       modeSet++;
-      modeSet %= numModes;
+      modeSet %= numMainStates;
       encoder.clearCount();
 
     } else if (modeEncPosition < 0) {
       modeSet--;
-      modeSet = (modeSet < 0) ? numModes - 1 : modeSet % numModes;
+      modeSet = (modeSet < 0) ? numMainStates - 1 : modeSet % numMainStates;
       encoder.clearCount();
     }
-    return modeSet;
   }
 
-  u_int32_t updateColorPosition() {
-    VolPosition = (encoder.getCount() / 2) % 0xFFFFFF;
-    return VolPosition;
+  void UpdateRgbStateSelect(int &modeSet) {
+
+    // checking encoder val state
+    modeEncPosition = encoder.getCount() / 2;
+    if (modeEncPosition == 0) {
+      // do nothing
+    } else if (modeEncPosition > 0) {
+      modeSet++;
+      modeSet %= numRgbStates;
+      encoder.clearCount();
+
+    } else if (modeEncPosition < 0) {
+      modeSet--;
+      modeSet = (modeSet < 0) ? numRgbStates - 1 : modeSet % numRgbStates;
+      encoder.clearCount();
+    }
   }
 
-  int getModeSelected() { return modeSelected; }
-  int getModeSet() { return modeSet; }
+  bool UpdateColorPickerSelect(uint8_t &modeSet) {
+
+    // checking encoder val state
+    modeEncPosition = encoder.getCount() / 2;
+    if (modeEncPosition == 0) {
+      // do nothing
+    } else if (modeEncPosition > 0) {
+      modeSet += colorPickerIncrement;
+      modeSet %= 255;
+      encoder.clearCount();
+      return true;
+
+    } else if (modeEncPosition < 0) {
+      modeSet -= colorPickerIncrement;
+      modeSet = (modeSet < 0) ? 0 : modeSet % 255;
+      encoder.clearCount();
+      return true;
+    }
+    return false;
+  }
+
+  bool EncButtonPressed() {
+    // check if the button was pressed
+    if (digitalRead(encBTN) == LOW && !ecnBtnPressed) {
+      delay(encButtonDebounce);
+      ecnBtnPressed = true;
+      return true;
+    } else if (digitalRead(encBTN) == HIGH && ecnBtnPressed) {
+      ecnBtnPressed = false;
+    }
+    return false;
+  }
 
 private:
   ESP32Encoder encoder;
 
-  // Dspl mode vareables
   long modeEncPosition = 0;
-  int modeSet = 0;      // which mode is currently active?
-  int modeSelected = 0; // Mode selected via pushbutton. default 0 == mainScreen
-  const int numModes = 5; // how many modes are available
+  int encButtonDebounce = 15; // mS
+  uint8_t colorPickerIncrement = 5;
 
   long VolPosition = 0;
   u_int32_t colorPosition = 0;
